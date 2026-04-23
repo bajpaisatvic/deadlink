c32b98e54c0508cd8f2f548581214ec0f77db0ff
Satvic Bajpai <satvicbajpai@gmail.com>
2026-04-24 03:13:27 +0530

    test: fix context load tests for checker and alert services

--- crawler-service/src/test/java/com/example/crawler_service/CrawlerServiceApplicationTests.java
+++ crawler-service/src/test/java/com/example/crawler_service/CrawlerServiceApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.crawler_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+		KafkaAutoConfiguration.class,
+		DataSourceAutoConfiguration.class,
+		FlywayAutoConfiguration.class
+})
 class CrawlerServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
